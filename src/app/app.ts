7ccceb37cc668abb98273b6538ad8dc9d92c37b2
hazeliscoding <hazel.granados@protonmail.com>
2026-01-13 10:52:45 -0600

    Adds home page and route as landing

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,12 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Home } from './pages/home/home';
+import { RouterOutlet } from "../../node_modules/@angular/router/types/_router_module-chunk";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [Home, RouterOutlet],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.css',
 })
 export class App {
   protected readonly title = signal('kawaii-linktree');
